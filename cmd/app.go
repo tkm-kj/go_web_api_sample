d853d5c3d441c9e60c0d47f8939e0a6737dcd05b
Takumi Kaji <ivezuki3@gmail.com>
2020-05-28 22:05:47 +0900

    GAEにデプロイできるようになった
    DBには繋がってないので、helloだけ出せる状況。

---
+++ cmd/app.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/tkm-kj/go_web_api_sample/internal/routes"
+)
+
+func main() {
+	e := echo.New()
+
+	routes.Register(e)
+	http.Handle("/", e)
+
+	port := os.Getenv("port")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
