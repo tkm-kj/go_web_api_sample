baa794141ea8551bbef62d6a6975d75ec1e05f3b
Takumi Kaji <ivezuki3@gmail.com>
2020-06-01 01:55:36 +0900

    deleted_at は null許可させたいので、 datetime として持たせるようにした

--- migrations/20200516093852-create_posts.sql
+++ migrations/20200516093852-create_posts.sql
@@ -4,7 +4,7 @@ CREATE TABLE posts (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(256) NOT NULL,
     body TEXT NOT NULL,
-    deleted_at timestamp,
+    deleted_at datetime,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (id)
--- migrations/20200516095814-create_comments.sql
+++ migrations/20200516095814-create_comments.sql
@@ -5,7 +5,7 @@ CREATE TABLE comments (
     post_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     body TEXT NOT NULL,
-    deleted_at timestamp,
+    deleted_at datetime,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (id),
